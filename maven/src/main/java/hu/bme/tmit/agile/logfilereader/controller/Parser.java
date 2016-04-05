6175ad432ff3cbaeb9825439db749e1868673059
aboross <boross.adam@gmail.com>
2016-04-05 18:54:44 +0200

    parser++

--- maven/src/main/java/hu/bme/tmit/agile/logfilereader/controller/Parser.java
+++ maven/src/main/java/hu/bme/tmit/agile/logfilereader/controller/Parser.java
@@ -21,12 +21,56 @@ public class Parser {
 	public void parse(String relativePath) {
 		applyRules();
 		try {
-			for (String string : Files.readAllLines((new File(relativePath).toPath()))) {
-				System.out.println(string);
+			for (String line : Files.readAllLines((new File(relativePath).toPath()))) {
+				String words[] = line.split(" ");
+				if(matchesDate(words[0])) {
+					String dateString = words[0];
+					String timeString = words[1];
+//					Date date = parseDate(dateString, timeString);
+					String sender = words[2];
+					if(isTimerOperation(words[3])) {
+						parseTimer(words);
+					}
+				}
 			}
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	private void parseTimer(String[] words) {
+		String name = null, duration = null;
+		if(isTimerStarted(words[5])) {
+			name = words[7].substring(0, words[7].length()-1);
+			duration = words[8];
+		}
+		else if(isTimerStopped(words[5])) {
+			name = words[7].substring(0, words[7].length()-1);
+			duration = words[8];
+		}
+		else if(isTimeout(words[5])) {
+			name = words[6].substring(0, words[6].length()-1);
+			duration = words[7];
+		}
+	}
+
+	private boolean isTimerStarted(String word) {
+		return word.equals("Start");
+	}
+	
+	private boolean isTimerStopped(String word) {
+		return word.equals("Stop");
+	}
+	
+	private boolean isTimeout(String word) {
+		return word.equals("Timeout");
+	}
+
+	private boolean isTimerOperation(String words) {
+		return words.equals("TIMEROP");
+	}
+
+	public boolean matchesDate(String date) {
+		return date.matches(RegexpPatterns.datePattern);
 	}
 }
---
+++ maven/src/main/java/hu/bme/tmit/agile/logfilereader/controller/ParserTest.java
@@ -0,0 +1,59 @@
+package hu.bme.tmit.agile.logfilereader.controller;
+
+import static org.junit.Assert.*;
+
+import java.util.Properties;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import util.PropertyHandler;
+
+public class ParserTest {
+	
+	Parser parser = new Parser();
+	String date = new String("2014/Oct/24");
+	String dateParts[];
+	
+	private static final String DATE_PROPERTY = "date";
+	private static final String REGEXP_PATTERNS_PROPERTIES = "regexp_patterns.properties";
+	
+	private void applyRules() {
+		PropertyHandler ph = new PropertyHandler();
+		Properties properties = ph.getProperties(REGEXP_PATTERNS_PROPERTIES);
+		RegexpPatterns.datePattern = properties.getProperty(DATE_PROPERTY);
+	}
+	
+	
+	@Before
+	public void setUp() {
+		dateParts = date.split("/");
+		applyRules();
+	}
+	
+	@Test
+	public void matchesYear() {
+		assertTrue(dateParts[0].matches("[1-2]\\d\\d\\d"));
+	}
+	
+	@Test
+	public void matchesMonth() {
+		assertTrue(dateParts[1].matches("\\D\\D\\D"));
+	}
+	
+	@Test
+	public void matchesDay() {
+		assertTrue(dateParts[2].matches("[0-3]\\d"));
+	}
+	
+	@Test
+	public void matchesDate() {
+		assertTrue(date.matches("[1-2]\\d\\d\\d/\\D\\D\\D/[0-3]\\d"));
+	}
+	
+	@Test
+	public void matchesDateFromProperties() {
+		assertTrue(parser.matchesDate(date));
+	}
+
+}
