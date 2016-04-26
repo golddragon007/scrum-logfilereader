9b03df1d96d38b3ff007fa8b4ccef178122b17dd
gmartin555 <gmartin555@gmail.com>
2016-04-26 14:51:37 +0200

    Regex pattern sent and receive message

--- maven/src/main/java/util/RegexpPatterns.java
+++ maven/src/main/java/util/RegexpPatterns.java
@@ -4,4 +4,6 @@ public abstract class RegexpPatterns {
 	public static String datePattern;
 	public static String verdictData;
 	public static String timePattern;
+	public static String sentPattern;
+	public static String receivePattern;
 }
