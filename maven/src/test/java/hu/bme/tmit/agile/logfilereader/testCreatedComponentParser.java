f0e5a7a42602c1fd78e08599ce33a301c1cfede6
koszta_adam <koszta6@gmail.com>
2016-05-05 13:21:23 +0200

    testTerminatedComponentParser & testCreatedComponentParser

---
+++ maven/src/test/java/hu/bme/tmit/agile/logfilereader/testCreatedComponentParser.java
@@ -0,0 +1,24 @@
+package hu.bme.tmit.agile.logfilereader;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import hu.bme.tmit.agile.logfilereader.controller.CreatedComponentParser;
+import hu.bme.tmit.agile.logfilereader.controller.TerminatedComponentParser;
+import hu.bme.tmit.agile.logfilereader.model.ComponentEvent.ComponentType;
+
+public class testCreatedComponentParser {
+	
+	@Test
+	public void testCreatedComponent() {
+		String testString = "2014/Oct/24 19:53:04.996229 hc PARALLEL - PTC was created. Component reference: 2456, component type: SipClientComponent.sipClientComponent, testcase name: WCG100200010, process id: 17927.";
+		String testParts[] = testString.split(" ");
+		assertEquals(CreatedComponentParser.parseCreatedComponent(testParts).getComponentReference(), 2456);
+		assertEquals(CreatedComponentParser.parseCreatedComponent(testParts).getComponentType(), "SipClientComponent.sipClientComponent");
+		assertEquals(CreatedComponentParser.parseCreatedComponent(testParts).getTestcaseName(), "WCG100200010");
+		assertEquals(CreatedComponentParser.parseCreatedComponent(testParts).getProcessID(), 17927);		
+		assertEquals(CreatedComponentParser.parseCreatedComponent(testParts).getCompType(), ComponentType.Create);
+	}
+
+}
---
+++ maven/src/test/java/hu/bme/tmit/agile/logfilereader/testTerminatedComponentParser.java
@@ -0,0 +1,21 @@
+package hu.bme.tmit.agile.logfilereader;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import hu.bme.tmit.agile.logfilereader.controller.CreatedComponentParser;
+import hu.bme.tmit.agile.logfilereader.controller.TerminatedComponentParser;
+import hu.bme.tmit.agile.logfilereader.model.ComponentEvent.ComponentType;
+
+public class testTerminatedComponentParser {
+	
+	@Test
+	public void testTerminatedComponent() {
+		String testString = "2014/Oct/24 19:53:07.338123 2456 PARALLEL - Terminating component type SipClientComponent.sipClientComponent.";
+		String testParts[] = testString.split(" ");
+		assertEquals(TerminatedComponentParser.parseTerminatedComponent(testParts).getComponentType(), "SipClientComponent.sipClientComponent");
+		assertEquals(TerminatedComponentParser.parseTerminatedComponent(testParts).getCompType(), ComponentType.Terminate);
+	}
+
+}
